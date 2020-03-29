944b9e0cd547944959cb4ef2c0b9b871c770b0fe
Hilla <hilla.guz@gmail.com>
2020-03-29 16:58:02 +0200

    Initial Commit

---
+++ CountWater/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CountWater
+//
+//  Created by Hilla on 3/29/20.
+//  Copyright © 2020 Hilla Guz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
